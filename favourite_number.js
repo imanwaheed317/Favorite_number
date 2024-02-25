d5a25eb903970664d6effafa450c713b24d02c23
imanwaheed317 <160356928+imanwaheed317@users.noreply.github.com>
2024-02-26 00:11:25 +0500

    Add files via upload

---
+++ favourite_number.js
@@ -0,0 +1,4 @@
+"use strict";
+const Favourite_number = 9;
+const message = 'Here is my favourite number ' + Favourite_number;
+console.log(message);
---
+++ favourite_number.ts
@@ -0,0 +1,6 @@
+
+const Favourite_number :number = 9
+
+const message = 'Here is my favourite number ' + Favourite_number;
+
+console.log(message);
